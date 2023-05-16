3300cfae2d9206240f1fc13a7446f4c240b9cf44
beomsic <qjatjr29@khu.ac.kr>
2023-05-16 19:36:03 +0900

    :sparkles: feat: redis config 클래스 추가

---
+++ src/main/java/numble/pet/vote/config/RedisConfig.java
@@ -0,0 +1,39 @@
+package numble.pet.vote.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableRedisRepositories
+@EnableConfigurationProperties(RedisProperties.class)
+public class RedisConfig {
+
+  private final RedisProperties redisProperties;
+
+  public RedisConfig(RedisProperties redisProperties) {
+    this.redisProperties = redisProperties;
+  }
+
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory() {
+    return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+  }
+
+  @Bean
+  public RedisTemplate<String, Object> redisTemplate() {
+    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+
+    redisTemplate.setConnectionFactory(redisConnectionFactory());
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+    return redisTemplate;
+  }
+
+}
