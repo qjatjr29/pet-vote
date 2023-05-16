368134960e8ee9d1060513e1a037d3c4b87e6a4b
beomsic <qjatjr29@khu.ac.kr>
2023-05-16 23:44:51 +0900

    :sparkles: feat: event 퍼블리셔를 전달하기 위한 설정 클래스 추가

---
+++ src/main/java/numble/pet/vote/config/EventsConfig.java
@@ -0,0 +1,22 @@
+package numble.pet.vote.config;
+
+import numble.pet.vote.common.event.Events;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EventsConfig {
+
+  private final ApplicationContext applicationContext;
+
+  public EventsConfig(ApplicationContext applicationContext) {
+    this.applicationContext = applicationContext;
+  }
+
+  @Bean
+  public InitializingBean eventsInitializer() {
+    return () -> Events.setPublisher(applicationContext);
+  }
+}
