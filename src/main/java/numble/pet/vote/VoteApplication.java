a8ae827f675231f3d97b62ad3cbbc2249cae6dae
beomsic <qjatjr29@khu.ac.kr>
2023-05-18 12:25:31 +0900

    :sparkles: feat: 캐시를 사용하기 위한 @EnableCaching 추가

--- src/main/java/numble/pet/vote/VoteApplication.java
+++ src/main/java/numble/pet/vote/VoteApplication.java
@@ -2,8 +2,10 @@ package numble.pet.vote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class VoteApplication {
 
 	public static void main(String[] args) {
