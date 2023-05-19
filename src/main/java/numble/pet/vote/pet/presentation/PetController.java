b8b88e624ebcd442030bbb3f7ff0d985ce4a3af5
beomsic <qjatjr29@khu.ac.kr>
2023-05-19 13:27:46 +0900

    :sparkles: feat: pet 상세 조회 로직 추가

--- src/main/java/numble/pet/vote/pet/presentation/PetController.java
+++ src/main/java/numble/pet/vote/pet/presentation/PetController.java
@@ -1,6 +1,7 @@
 package numble.pet.vote.pet.presentation;
 
 import numble.pet.vote.pet.command.application.PetService;
+import numble.pet.vote.pet.query.application.PetDetailResponse;
 import numble.pet.vote.pet.query.application.PetQueryService;
 import numble.pet.vote.pet.query.application.PetSummaryResponse;
 import org.springframework.data.domain.Page;
@@ -8,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +29,12 @@ public class PetController {
   @GetMapping
   public ResponseEntity<Page<PetSummaryResponse>> findAllPets(@PageableDefault(page = 0, size = 15) Pageable pageable) {
     Page<PetSummaryResponse> response = petQueryService.findAllPets(pageable);
+    return ResponseEntity.ok(response);
+  }
+
+  @GetMapping("/{petId}")
+  public ResponseEntity<PetDetailResponse> findPetById(@PathVariable Long petId) {
+    PetDetailResponse response = petQueryService.findPetById(petId);
     return ResponseEntity.ok(response);
   }
 
--- src/main/java/numble/pet/vote/pet/query/application/PetQueryService.java
+++ src/main/java/numble/pet/vote/pet/query/application/PetQueryService.java
@@ -1,6 +1,7 @@
 package numble.pet.vote.pet.query.application;
 
 import numble.pet.vote.pet.query.domain.PetQueryRepository;
+import numble.pet.vote.pet.query.domain.PetData;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,13 @@ public class PetQueryService {
 
   public Page<PetSummaryResponse> findAllPets(Pageable pageable) {
     return petQueryRepository.findAll(pageable).map(PetSummaryResponse::of);
+  }
+
+  public PetDetailResponse findPetById(Long id) {
+    PetData petData = petQueryRepository.findById(id)
+        .orElseThrow(RuntimeException::new);
+
+    return PetDetailResponse.of(petData);
   }
 
 }
