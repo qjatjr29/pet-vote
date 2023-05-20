a0c69ee477e0a5b4e5600297e5808a98a848c8a7
beomsic <qjatjr29@khu.ac.kr>
2023-05-20 11:38:00 +0900

    :sparkles: feat: pet 종류에 대한 string -> Enum static 메서드 추가

--- src/main/java/numble/pet/vote/pet/command/domain/Species.java
+++ src/main/java/numble/pet/vote/pet/command/domain/Species.java
@@ -3,7 +3,15 @@ package numble.pet.vote.pet.command.domain;
 public enum Species {
 
   강아지,
-  고양이;
+  고양이,
+  ETC;
 
+  public static Species of(String species) {
+    try {
+      return Species.valueOf(species);
+    } catch (IllegalArgumentException e) {
+      return Species.ETC;
+    }
+  }
 
 }
