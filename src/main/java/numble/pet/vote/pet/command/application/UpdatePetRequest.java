b5bd69f39bf8f3f8dc8f03bc532c3ff824ce4802
beomsic <qjatjr29@khu.ac.kr>
2023-05-20 11:53:39 +0900

    :sparkles: feat: pet update dto 추가

---
+++ src/main/java/numble/pet/vote/pet/command/application/UpdatePetRequest.java
@@ -0,0 +1,18 @@
+package numble.pet.vote.pet.command.application;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class UpdatePetRequest {
+
+  private String name;
+  private String Species;
+  private String description;
+  private String image;
+
+}
