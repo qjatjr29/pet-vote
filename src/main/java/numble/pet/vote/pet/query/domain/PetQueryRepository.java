c7f0b79a493be320d5aa537f880d0f25404a227b
beomsic <qjatjr29@khu.ac.kr>
2023-05-20 13:38:10 +0900

    :sparkles: feat: Pet 조회모델에서 petId를 통해 데이터 조회 기능 추가

--- src/main/java/numble/pet/vote/pet/query/domain/PetQueryRepository.java
+++ src/main/java/numble/pet/vote/pet/query/domain/PetQueryRepository.java
@@ -1,11 +1,15 @@
 package numble.pet.vote.pet.query.domain;
 
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface PetQueryRepository extends MongoRepository<PetData, Long> {
 
   Page<PetData> findAll(Pageable pageable);
 
+  @Query("{ 'petId' : ?0 }")
+  Optional<PetData> findByPetId(Long petId);
 }
