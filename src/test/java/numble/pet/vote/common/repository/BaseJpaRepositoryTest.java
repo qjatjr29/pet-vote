54f36141a4a05f9c1f629dc7ce6d0060387bef1d
beomsic <qjatjr29@khu.ac.kr>
2023-05-24 15:32:14 +0900

    :test_tube: feat: spring data jpa를 테스트시 기본이 되는 클래스

---
+++ src/test/java/numble/pet/vote/common/repository/BaseJpaRepositoryTest.java
@@ -0,0 +1,13 @@
+package numble.pet.vote.common.repository;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class BaseJpaRepositoryTest {
+
+}
