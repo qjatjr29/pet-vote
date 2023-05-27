b286a9486c07f74f05ab23a854ec696ba458482a
beomsic <qjatjr29@khu.ac.kr>
2023-05-27 14:32:33 +0900

    :recycle: refactor : @Query 사용하지 않도록 변경

--- src/main/java/numble/pet/vote/vote/command/domain/VoteRepository.java
+++ src/main/java/numble/pet/vote/vote/command/domain/VoteRepository.java
@@ -1,13 +1,10 @@
 package numble.pet.vote.vote.command.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface VoteRepository extends JpaRepository<Vote, Long> {
 
-  @Query("SELECT v FROM Vote v WHERE v.email =:email")
-  Boolean existsByEmail(@Param("email") String email);
+  Boolean existsByEmail(String email);
 
   void deleteByEmail(String email);
 }
